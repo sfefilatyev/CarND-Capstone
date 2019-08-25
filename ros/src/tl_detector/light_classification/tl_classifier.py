3c66499a91b8ab8ddce63521bbd60fd8e26c71b6
Dmitriy Litvak <dlitvak@tibco.com>
2019-08-25 15:41:55 -0700

    - put in tensorflow import - fix the tl_classifier bugs - add logging

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -3,14 +3,22 @@ import rospy
 import tensorflow
 import numpy as np
 import time
+import os
+import errno
+from PIL import Image
 
 class TLClassifier(object):
-    def __init__(self, tl_model_dir, min_detect_score_thresh=0.5):
+    def __init__(self, tl_model_path, min_detect_score_thresh=0.5):
         # load classifier
-        rospy.loginfo('>>> TLClassifier is using model dir: %s, min TL detection threshold: %s', tl_model_dir, str(min_detect_score_thresh))
+        tl_model_file_path = os.getcwd() + '/' + tl_model_path           # os.getcwd() points to tl_detector.py dir where the code exec
+        rospy.loginfo('>>> TLClassifier is using model at: %s, min TL detection threshold: %s', tl_model_file_path, str(min_detect_score_thresh))
+
+        if not os.path.exists(tl_model_file_path) or not os.path.isfile(tl_model_file_path):
+            rospy.logerror('Cannot find the model file configured in traffic light configs yaml.')
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), tl_model_file_path)
 
         # load NN graph
-        self.detection_graph = self.load_graph(tl_model_dir + '/frozen_inference_graph.pb')
+        self.detection_graph = self.load_graph(tl_model_file_path)
         self.min_detect_thresh = min_detect_score_thresh
 
         self.categoty_to_tl_map = {'1': TrafficLight.GREEN, '2': TrafficLight.RED, '3': TrafficLight.YELLOW, '4': TrafficLight.UNKNOWN}
@@ -45,10 +53,11 @@ class TLClassifier(object):
                 t1 = time.time()
 
                 traff_light = TrafficLight.UNKNOWN
-                tl_col_str = self.categoty_to_str_map[4]
+                detect_score = 0
+                tl_col_str = self.categoty_to_str_map['4']
                 if scores is not None and len(scores[0]) > 0:
-                    highest_score = np.squeeze(scores)[0]
-                    if highest_score > self.min_detect_thresh:
+                    detect_score = np.squeeze(scores)[0]
+                    if detect_score > self.min_detect_thresh:
                         # A positive detection happened
                         cat_idx_str = str(np.squeeze(classes).astype(np.int32)[0])
                         tl_col_str = self.categoty_to_str_map[cat_idx_str]
@@ -56,7 +65,7 @@ class TLClassifier(object):
 
                 rospy.logdebug('----------------------')
 
-                rospy.loginfo('TLClassifier detected %s (%s) light after %d msec', tl_col_str, str(traff_light), (t1 - t0) * 1000)
+                rospy.loginfo('TLClassifier detected %s (score %s) light after %d msec', tl_col_str, str(detect_score), (t1 - t0) * 1000)
 
                 rospy.logdebug('TLClassifier SCORES %s', str(scores[0]))
                 rospy.logdebug('TLClassifier CLASSES %s', str(classes[0]))
@@ -76,5 +85,6 @@ class TLClassifier(object):
         return graph
 
     def load_image_into_numpy_array(self, image):
+        image = Image.fromarray(image)
         (im_width, im_height) = image.size
         return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
